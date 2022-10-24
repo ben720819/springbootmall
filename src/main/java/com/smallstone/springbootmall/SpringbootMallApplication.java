9f6668eed091446550830706f35a44f5c9e3e837
ben720819 <115257290+ben720819@users.noreply.github.com>
2022-10-25 01:14:47 +0800

    Update SpringbootMallApplication.java

--- src/main/java/com/smallstone/springbootmall/SpringbootMallApplication.java
+++ src/main/java/com/smallstone/springbootmall/SpringbootMallApplication.java
@@ -3,10 +3,12 @@ package com.smallstone.springbootmall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@ComponentScan({"com.smallstone.springbootmall.controller"})
 @RestController
 
 public class SpringbootMallApplication {
