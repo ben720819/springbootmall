e8a7ee0cbe02467c5aa392dc4350f4ae39c5040e
ben720819 <115257290+ben720819@users.noreply.github.com>
2022-10-24 21:57:27 +0800

    Update UserController.java

--- src/main/java/com/smallstone/springbootmall/controller/UserController.java
+++ src/main/java/com/smallstone/springbootmall/controller/UserController.java
@@ -21,10 +21,6 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-    
-    @RequestMapping("/hello")
-    public String hello(){
-        return "HelloWorld";}
 
     @PostMapping("users/register")
     public ResponseEntity<User> register(@RequestBody @Valid UserRegisterRequest userRegisterRequest)
