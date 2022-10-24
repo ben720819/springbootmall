0dab33824c9001fe106291ec5b3ae8ee99829092
ben720819 <115257290+ben720819@users.noreply.github.com>
2022-10-25 00:26:44 +0800

    Update UserController.java

--- src/main/java/com/smallstone/springbootmall/controller/UserController.java
+++ src/main/java/com/smallstone/springbootmall/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
     @Autowired
     private UserService userService;
     
-    @RequestMapping("/users/register")
+    @PostMapping("/users/register")
     public ResponseEntity<User> register(@RequestBody @Valid UserRegisterRequest userRegisterRequest)
     {
         Integer userId = userService.register(userRegisterRequest);
@@ -34,7 +34,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
-    @RequestMapping("/users/login")
+    @PostMapping("/users/login")
     public ResponseEntity<User> lgoin(@RequestBody @Valid UserLoginRequest userLoginRequest)
     {
         User user = userService.login(userLoginRequest);
