0293f300ac855bdbb89d76934d880819049cd344
ben720819 <115257290+ben720819@users.noreply.github.com>
2022-10-25 00:14:48 +0800

    Update SpringbootMallApplication.java

--- src/main/java/com/smallstone/springbootmall/SpringbootMallApplication.java
+++ src/main/java/com/smallstone/springbootmall/SpringbootMallApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = "com.smallstone.springbootmall.*")
+@SpringBootApplication(scanBasePackages = "com.smallstone.springbootmall.controller")
 
 public class SpringbootMallApplication {
 
