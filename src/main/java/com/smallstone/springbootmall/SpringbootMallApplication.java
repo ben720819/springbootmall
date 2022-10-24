c979f107266f41ca55f4e3044f710737f5e4b597
ben720819 <115257290+ben720819@users.noreply.github.com>
2022-10-25 00:54:16 +0800

    Update SpringbootMallApplication.java

--- src/main/java/com/smallstone/springbootmall/SpringbootMallApplication.java
+++ src/main/java/com/smallstone/springbootmall/SpringbootMallApplication.java
@@ -3,14 +3,15 @@ package com.smallstone.springbootmall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 
 public class SpringbootMallApplication {
-    
+    @RequestMapping("/")
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMallApplication.class, args);
     }
